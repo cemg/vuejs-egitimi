5ee3f9023995f3983590937d950f047106c2d583
Cem Gündüzoğlu <cmg.web@gmail.com>
2018-08-10 23:26:06 +0300

    Bölüm 3 Cli projesi eklendi

---
+++ B3/cli-proje/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <hello msg="Welcome to Your Vue.js App"/>
+        <alert-count></alert-count>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+    import AlertCount from './components/AlertCount.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            hello: HelloWorld,
+            'alert-count': AlertCount
+        }
+    }
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ B3/cli-proje/src/components/AlertCount.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="alert alert-success">
+        Merhaba
+        <button @click="count++" class="btn btn-sm btn-primary">
+            {{ count }} kez tıkladın
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        }
+    }
+</script>
