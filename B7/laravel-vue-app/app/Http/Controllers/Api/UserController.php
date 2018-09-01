f9cee43141230421043f65d7b68a48f1d30e26fb
Cem Gündüzoğlu <cmg.web@gmail.com>
2018-09-02 01:20:10 +0300

    Şifre alanı boş bırakıldığında bu alanı güncelleme işlemine dahil etmeden doğrulama ve güncelleme yapması ayarlandı.

--- B7/laravel-vue-app/app/Http/Controllers/Api/UserController.php
+++ B7/laravel-vue-app/app/Http/Controllers/Api/UserController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -18,30 +18,31 @@ class UserController extends Controller
     public function index()
     {
         $users = UserResource::collection(User::orderByDesc('id')->paginate(5));
+        
         return $users;
         
         //return response()->json(['users' => $users], 200);
     }
-
+    
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-           'name' => 'required|max:50',
-           'email' => 'required|email|unique:users',
-           'password' => 'required|min:6'
+            'name'     => 'required|max:50',
+            'email'    => 'required|email|unique:users',
+            'password' => 'required|min:6'
         ]);
         
         if ($validator->fails()) {
             return response()->json([
                 'success' => false,
                 'message' => 'Bir hata oluştu',
-                'errors' => $validator->errors()
+                'errors'  => $validator->errors()
             ], 422);
         }
         
@@ -49,22 +50,23 @@ class UserController extends Controller
         $data['password'] = bcrypt($data['password']);
         
         User::create($data);
-    
+        
         return response()->json([
             'success' => true,
             'message' => 'Kaydedildi'
         ]);
     }
-
+    
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
         $user = new UserResource(User::find($id));
+        
         return response()->json($user, 200);
         
         //return User::find($id);
@@ -79,25 +81,33 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:50',
-            'email' => 'required|email|unique:users,email,' . $user->id,
-            'password' => 'required|min:6'
-        ]);
-    
+        if (request()->filled('password')) {
+            $validator = Validator::make($request->all(), [
+                'name'     => 'required|max:50',
+                'email'    => 'required|email|unique:users,email,' . $user->id,
+                'password' => 'required|min:6'
+            ]);
+        } else {
+            $validator = Validator::make($request->all(), [
+                'name'  => 'required|max:50',
+                'email' => 'required|email|unique:users,email,' . $user->id
+            ]);
+        }
+        
         if ($validator->fails()) {
             return response()->json([
                 'success' => false,
                 'message' => 'Bir hata oluştu',
-                'errors' => $validator->errors()
+                'errors'  => $validator->errors()
             ], 422);
         }
-    
+        
         $data = $request->only('name', 'email', 'password');
-        $data['password'] = bcrypt($data['password']);
-    
+        if (request()->filled('password')) {
+            $data['password'] = bcrypt($data['password']);
+        }
         $user->update($data);
-    
+        
         return response()->json([
             'success' => true,
             'message' => 'Güncellendi'
@@ -114,7 +124,7 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         $user->delete();
-    
+        
         return response()->json([
             'success' => true,
             'message' => 'Silindi'
